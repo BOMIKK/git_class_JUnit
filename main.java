faa3dba5ff2477dc532242e6dbe822f25aac5df9
BOM2 <36789853+BOMIKK@users.noreply.github.com>
2019-12-05 12:15:17 +0900

    Add files via upload

---
+++ main.java
@@ -0,0 +1,15 @@
+package test;
+
+//local by BOMIKK
+//fetch test1
+//1. localBranch Test
+
+public class main {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated constructor stub
+		System.out.println("Test");
+	}
+
+	
+}
